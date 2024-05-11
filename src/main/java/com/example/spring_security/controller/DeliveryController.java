55ca8ddd119316a1e402ffbf02f19139045d4eea
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-05-11 16:00:27 +0300

    search fix

--- src/main/java/com/example/spring_security/controller/DeliveryController.java
+++ src/main/java/com/example/spring_security/controller/DeliveryController.java
@@ -1,56 +1,4 @@
-//package com.example.spring_security.controller;
-//
-//import com.example.spring_security.entity.Delivery;
-//import com.example.spring_security.entity.DeliveryType;
-//import com.example.spring_security.entity.ShoppingCart;
-//import com.example.spring_security.service.DeliveryService;
-//import com.example.spring_security.service.ShoppingCartService;
-//import jakarta.servlet.http.HttpSession;
-//import lombok.AllArgsConstructor;
-//import org.springframework.ui.Model;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//import java.util.List;
-//
-//@Controller
-//@RequestMapping("/deliveries")
-//@SessionAttributes("cart")
-//@AllArgsConstructor
-//public class DeliveryController {
-//    private final ShoppingCartService shoppingCartService;
-//    private final DeliveryService deliveryService;
-//
-//    @GetMapping("/create")
-//    public String createDeliveryForm(HttpSession session, Model model) {
-//        ShoppingCart cart = shoppingCartService.getCartFromSession(session);
-//
-//        model.addAttribute("delivery", new Delivery()); // Объект для новой доставки
-//        model.addAttribute("cart", cart); // Передаем корзину в шаблон
-//
-//        return "create_delivery"; // Шаблон для создания новой доставки
-//    }
-//
-//
-//
-//    @PostMapping("/createFromCart")
-//    public String createDeliveryFromCart(HttpSession session, Model model, @RequestParam("productIds") List<Long> productIds) {
-//        ShoppingCart cart = shoppingCartService.getCartFromSession(session); // Получаем корзину из сессии
-//        if (cart == null || cart.getItems().isEmpty()) {
-//            return "redirect:/cart"; // Если корзина пуста, перенаправляем обратно
-//        }
-//
-//        // Создаем новую доставку, используя корзину и переданные идентификаторы продуктов
-//        Delivery newDelivery = deliveryService.createDeliveryFromCart(cart, productIds);
-//        model.addAttribute("delivery", newDelivery); // Передаем созданную доставку в модель
-//
-//        // Очищаем корзину после создания доставки
-//        shoppingCartService.clearCart(cart);
-//
-//        return "redirect:/deliveries"; // Перенаправляем к списку доставок
-//    }
-//
 package com.example.spring_security.controller;
-
 import com.example.spring_security.entity.Delivery;
 import com.example.spring_security.entity.ShoppingCart;
 import com.example.spring_security.service.DeliveryService;
@@ -103,7 +51,13 @@ public class DeliveryController {
     }
 
 
-    // Другие методы для создания, обновления и удаления доставок
+    @PostMapping("/delete/{id}")
+    public String deleteDeliveryById(@PathVariable Long id) {
+        deliveryService.deleteDelivery(id);
+        return "redirect:/deliveries"; // Перенаправляем пользователя на список доставок
+    }
+
+
 }
 
 
